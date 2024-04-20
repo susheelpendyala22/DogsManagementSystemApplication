76142e26678af3dd81a124f99e92fc2acfc9fe7c
Susheel_Pendyala <pendyalasusheel@gmail.com>
2024-04-19 23:38:49 -0500

    commit to main

--- src/main/java/com/example/DMS/repository/TrainerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.DMS.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.DMS.Models.Trainer;
-
-public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
-
-}
